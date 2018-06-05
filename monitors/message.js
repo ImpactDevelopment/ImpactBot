79206a47de956a072008eaab19533bb5d47970f3
SuperOP535 <27556391+SuperOP535@users.noreply.github.com>
2018-06-05 18:01:27 +0000

    Add exclude array, delete message after 15 sec

--- monitors/message.js
+++ monitors/message.js
@@ -2,16 +2,17 @@ const { Monitor } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 
 const list = [
-  [/4\.3|forge|optifine|installer/, 'You\'ve mentioned an upcoming feature, please wait for it and check the faq or upcoming chann'],
-  [/(web)?(site|page)|donate|become ?a? don(at)?or/, 'Check out the [website](https://impactdevelopment.github.io/) :)'],
-  [/issue|bug|crash|error|suggestion|feature|enhancement/, 'Use the [GitHub repo](https://github.com/ImpactDevelopment/ImpactClient/issues) to report issues/suggestions!'],
-  [/help|support|assistance/, 'Switch to the help channel!']
+  [/4\.3|forge|optifine|installer/, 'You\'ve mentioned an upcoming feature, check the faq and upcoming channels.', []],
+  [/(web)?(site|page)|donate|become ?a? don(at)?or/, 'Check out the [website](https://impactdevelopment.github.io/) :)', []],
+  [/issue|bug|crash|error|suggestion|feature|enhancement/, 'Use the [GitHub repo](https://github.com/ImpactDevelopment/ImpactClient/issues) to report issues/suggestions!', []],
+  [/help|support|assistance/, 'Switch to the help channel!', ['222120655594848256']],
+  [/franky/, 'It does exactly what you think it does.', []]
 ];
 
-function buildStr(str) {
+function buildStr(str, channelId) {
   let a = '';;
   list.forEach(e => {
-    if(e[0].test(str)) a += e[1] + ' ';
+    if(e[0].test(str) && !e[2].includes(channelId)) a += e[1] + ' ';
   });
   return a;
 }
@@ -29,8 +30,11 @@ module.exports = class extends Monitor {
     });
   }
 
-  run(msg) {
-    const str = buildStr(msg.content);
-    if(str) msg.send(new MessageEmbed().setDescription(str));
+  async run(msg) {
+    const str = buildStr(msg.content, msg.channel.id);
+    if(str) {
+      const m = await msg.send(new MessageEmbed().setDescription(str));
+      setTimeout(m.delete.bind(m), 15000);
+    }
   }
 };
