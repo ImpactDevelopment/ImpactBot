688aced6552a6051bb1214734cd0b8a066e4ae3a
LeafHacker <leafhacker@yahoo.com>
2018-06-09 11:44:26 +0100

    message.js case-insensitive match

--- monitors/message.js
+++ monitors/message.js
@@ -2,11 +2,11 @@ const { Monitor } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 
 const list = [
-  [/\b(4\.3|forge|optifine|installer)\b/, 'You\'ve mentioned an upcoming feature, <#398628237753843732> <#451094829393510420>.', []],
-  [/\b((web)?(site|page)|donate|become ?a? don(at)?or)\b/, 'Check out the [website](https://impactdevelopment.github.io/) :)', []],
-  [/\b(issue|bug|crash|error|suggest(ion)?|feature|enhancement)\b/, 'Use the [GitHub repo](https://github.com/ImpactDevelopment/ImpactClient/issues) to report issues/suggestions!', []],
-  [/\b(help|support|assistance)\b/, 'Switch to the <#222120655594848256> channel!', ['222120655594848256']],
-  [/\b(franky)\b/, 'It does exactly what you think it does.', []]
+  [/\b(4\.3|forge|optifine|installer)\b/i, 'You\'ve mentioned an upcoming feature, <#398628237753843732> <#451094829393510420>.', []],
+  [/\b((web)?(site|page)|donate|become ?a? don(at)?or)\b/i, 'Check out the [website](https://impactdevelopment.github.io/) :)', []],
+  [/\b(issue|bug|crash|error|suggest(ion)?|feature|enhancement)\b/i, 'Use the [GitHub repo](https://github.com/ImpactDevelopment/ImpactClient/issues) to report issues/suggestions!', []],
+  [/\b(help|support|assistance)\b/i, 'Switch to the <#222120655594848256> channel!', ['222120655594848256']],
+  [/\b(franky)\b/i, 'It does exactly what you think it does.', []]
 ];
 
 function buildStr(str, channelId) {
