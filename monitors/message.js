51f082414fca7339c597fd2fb82b405633934d97
LeafHacker <leafhacker@yahoo.com>
2018-06-09 11:55:36 +0100

    message.js buildStr() -> .filter().reduce()

--- monitors/message.js
+++ monitors/message.js
@@ -1,21 +1,13 @@
 const { Monitor } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 
-const list = [
+const replies = [
   [/\b(4\.3|forge|optifine|installer)\b/i, 'You\'ve mentioned an upcoming feature, <#398628237753843732> <#451094829393510420>.', []],
   [/\b((web)?(site|page)|donate|become ?a? don(at)?or)\b/i, 'Check out the [website](https://impactdevelopment.github.io/) :)', []],
   [/\b(issue|bug|crash|error|suggest(ion)?|feature|enhancement)\b/i, 'Use the [GitHub repo](https://github.com/ImpactDevelopment/ImpactClient/issues) to report issues/suggestions!', []],
   [/\b(help|support|assistance)\b/i, 'Switch to the <#222120655594848256> channel!', ['222120655594848256']],
   [/\b(franky)\b/i, 'It does exactly what you think it does.', []]
 ];
-
-function buildStr(str, channelId) {
-  let a = '';;
-  list.forEach(e => {
-    if(e[0].test(str) && !e[2].includes(channelId)) a += e[1] + ' ';
-  });
-  return a;
-}
 
 module.exports = class extends Monitor {
   constructor(...args) {
@@ -31,9 +23,12 @@ module.exports = class extends Monitor {
   }
 
   async run(msg) {
-    const str = buildStr(msg.content, msg.channel.id);
-    if(str) {
-      const m = await msg.send(new MessageEmbed().setDescription(str));
+    const reply = replies
+      .filter((e) => e[0].test(msg.content) && !e[2].includes(msg.channel.id))
+      .reduce((a, e) => a + e[1] + ' ');
+
+    if(reply) {
+      const m = await msg.send(new MessageEmbed().setDescription(reply));
       setTimeout(m.delete.bind(m), 15000);
     }
   }
