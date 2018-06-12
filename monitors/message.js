5f80e6b7b877966ea305037a85ece4953fbb15bc
SuperOP535 <27556391+SuperOP535@users.noreply.github.com>
2018-06-12 08:02:19 +0000

    \b is ugly

--- monitors/message.js
+++ monitors/message.js
@@ -2,11 +2,12 @@ const { Monitor } = require('klasa');
 const { MessageEmbed } = require('discord.js');
 
 const replies = [
-  [/\b(4\.3|forge|optifine|installer)\b/i, 'You\'ve mentioned an upcoming feature, <#398628237753843732> <#451094829393510420>.', []],
-  [/\b((web)?(site|page)|donate|become ?a? don(at)?or)\b/i, 'Check out the [website](https://impactdevelopment.github.io/) :)', []],
-  [/\b(issue|bug|crash|error|suggest(ion)?|feature|enhancement)\b/i, 'Use the [GitHub repo](https://github.com/ImpactDevelopment/ImpactClient/issues) to report issues/suggestions!', []],
-  [/\b(help|support|assistance)\b/i, 'Switch to the <#222120655594848256> channel!', ['222120655594848256']],
-  [/\b(franky)\b/i, 'It does exactly what you think it does.', []]
+  [/(4\.3|forge|installer)/, '<#398628237753843732> <#451094829393510420>.', []],
+  [/((web)?(site|page)|donate|become ?a? don(at)?or)/, '[Click here](https://impactdevelopment.github.io).', []],
+  [/(issue|bug|crash|error|suggest(ion)?|feature|enhancement)/, 'Use the [GitHub repo](https://github.com/ImpactDevelopment/ImpactClient/issues) to report issues/suggestions!', []],
+  [/(help|support|assistance)/, 'Switch to the <#222120655594848256> channel!', ['222120655594848256']],
+  [/(franky)/, 'It does exactly what you think it does.', []],
+  [/optifine/, '[4.0, 4.1](https://www.youtube.com/watch?v=o1LHq6L0ibk), 4.2: not compatible', []]
 ];
 
 module.exports = class extends Monitor {
@@ -24,7 +25,7 @@ module.exports = class extends Monitor {
 
   async run(msg) {
     const reply = replies
-      .filter((e) => e[0].test(msg.content) && !e[2].includes(msg.channel.id))
+      .filter((e) => new RegExp('\\b' + e[0].toString().slice(1, -1) + '\\b', 'i').test(msg.content) && !e[2].includes(msg.channel.id))
       .reduce((a, e) => a + e[1] + ' ', '');
 
     if(reply) {
