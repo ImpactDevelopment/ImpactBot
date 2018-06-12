c0b0fe20846af767f7d6b4e44731800e1a50e47e
Leaf <LeafHacker@users.noreply.github.com>
2018-06-12 23:06:03 +0100

    Fix brackets being removed

--- monitors/message.js
+++ monitors/message.js
@@ -25,7 +25,7 @@ module.exports = class extends Monitor {
 
   async run(msg) {
     const reply = replies
-      .filter((e) => new RegExp('\\b' + e[0].toString().slice(1, -1) + '\\b', 'i').test(msg.content) && !e[2].includes(msg.channel.id))
+      .filter((e) => new RegExp('\\b(?:' + e[0].toString().slice(1, -1) + ')\\b', 'i').test(msg.content) && !e[2].includes(msg.channel.id))
       .reduce((a, e) => a + e[1] + ' ', '');
 
     if(reply) {
