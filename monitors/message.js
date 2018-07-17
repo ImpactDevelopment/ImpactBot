f89b976c871b46690348dac89216d54aa0919707
LeafHacker <leafhacker@yahoo.com>
2018-07-17 19:21:25 +0100

    Only respond in #General and #Help

--- monitors/message.js
+++ monitors/message.js
@@ -5,7 +5,7 @@ const replies = [
   [/4\.3|forge|installer/, '<#398628237753843732> <#451094829393510420>.', []],
   [/(web)?(site|page)|donate|become ?a? don(at)?or/, '[Click here](https://impactdevelopment.github.io).', []],
   [/issue|bug|crash|error|suggest(ion)?|feature|enhancement/, 'Use the [GitHub repo](https://github.com/ImpactDevelopment/ImpactClient/issues) to report issues/suggestions!', []],
-  [/help|support|assistance/, 'Switch to the <#222120655594848256> channel!', ['222120655594848256']],
+  [/help|support|assistance/, 'Switch to the <#222120655594848256> channel!', [ /* Help */ '222120655594848256' ]],
   [/franky/, 'It does exactly what you think it does.', []],
   [/optifine/, '[4.0, 4.1](https://www.youtube.com/watch?v=o1LHq6L0ibk), 4.2: not compatible', []]
 ];
@@ -28,7 +28,7 @@ module.exports = class extends Monitor {
       .filter((e) => new RegExp('\\b(?:' + e[0].toString().slice(1, -1) + ')\\b', 'i').test(msg.content) && !e[2].includes(msg.channel.id))
       .reduce((a, e) => a + e[1] + ' ', '');
 
-    if(reply) {
+    if(reply && msg.channel.id in [ /* General */ '208753003996512258', /* Help */ '222120655594848256' ]) {
       const m = await msg.send(new MessageEmbed().setDescription(reply));
       setTimeout(m.delete.bind(m), 15000);
     }
