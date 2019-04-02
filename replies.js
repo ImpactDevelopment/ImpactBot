e1c9672f55d1482316dab4686188368368af5145
SuperOP535 <27556391+SuperOP535@users.noreply.github.com>
2019-04-02 17:24:36 +0200

    4.6, https, forge, remove memes, simplify

--- replies.js
+++ replies.js
@@ -3,22 +3,22 @@ const channels = require('./channels');
 module.exports = [
   {
     pattern: /forge/,
-    message: 'Soon™ <#' + channels.faq + '> <#' + channels.upcoming + '>.'
+    message: 'Very soon™ (<#' + channels.upcoming + '>).'
   },
   {
     pattern: /lite\s*loader/,
-    message: '[Click here for an Impact + LiteLoader tutorial!](https://github.com/ImpactDevelopment/ImpactClient/blob/master/LiteLoader.md)',
+    message: '[LiteLoader tutorial](https://github.com/ImpactDevelopment/ImpactClient/blob/master/LiteLoader.md)',
   },
   {
     pattern: /(web)?(site|page)|donate|become ?a? don(at)?or/,
-    message: '[Click here to open the website](https://impactdevelopment.github.io).'
+    message: '[Website](https://impactdevelopment.github.io)'
   },
   {
     pattern: /issue|bug|crash|error|suggest(ion)?s?|feature|enhancement/,
     message: 'Use the [GitHub repo](https://github.com/ImpactDevelopment/ImpactClient/issues) to report issues/suggestions!'
   },
   {
-    pattern: /help|support|assistance/,
+    pattern: /help|support/,
     message: 'Switch to the <#' + channels.help + '> channel!',
     exclude: [channels.help]
   },
@@ -28,34 +28,22 @@ module.exports = [
   },
   {
     pattern: /opti\s*fine/,
-    message: 'Optifine can be installed with Impact 4.0 and up (except 4.2). Instructions: [video](https://www.youtube.com/watch?v=o1LHq6L0ibk), [text](https://github.com/ImpactDevelopment/ImpactClient/blob/master/Optifine.md)'
+    message: '[OptiFine tutorial (4.0+ but not 4.2)](https://www.youtube.com/watch?v=o1LHq6L0ibk), [text](https://github.com/ImpactDevelopment/ImpactClient/blob/master/Optifine.md)'
   },
   {
-    pattern: /mediafire|die? ?re(c|k)+to? (linko?|url|site|page)|adf\.?ly|(ad|u)block/,
-    message: 'Direct links: [4.2](http://www.mediafire.com/file/ziqx4m44zkgj1ye/Impact+4.2+for+Minecraft+1.12.2.zip) | [4.3](http://www.mediafire.com/file/9ujvsouklxoq5hj/Impact+4.3+for+Minecraft+1.12.2.zip) | [4.4](http://www.mediafire.com/file/l7brss1f228so0p/Impact+4.4+for+Minecraft+1.12.2.zip) | [4.5](http://www.mediafire.com/file/a9srjpjfb4uppqj/Impact+4.5+for+Minecraft+1.12.2.zip).'
+    pattern: /mediafire|dire(c|k)+to? (linko?|url|site|page)|adf\.?ly|(ad|u)block/,
+    message: '[4.2](https://www.mediafire.com/file/ziqx4m44zkgj1ye/Impact+4.2+for+Minecraft+1.12.2.zip) | [4.3](https://www.mediafire.com/file/9ujvsouklxoq5hj/Impact+4.3+for+Minecraft+1.12.2.zip) | [4.4](https://www.mediafire.com/file/l7brss1f228so0p/Impact+4.4+for+Minecraft+1.12.2.zip) | [4.5](https://www.mediafire.com/file/a9srjpjfb4uppqj/Impact+4.5+for+Minecraft+1.12.2.zip). | [4.6](https://www.mediafire.com/file/ccjf2xs8veokkc9/Impact+4.6+for+Minecraft+1.13.2.zip)'
   },
   {
     pattern: /macros?/,
-    message: '[Manually creating macros](https://github.com/ImpactDevelopment/ImpactClient/issues/153#issuecomment-399772723)'
+    message: '[Macro tutorial](https://github.com/ImpactDevelopment/ImpactClient/issues/153#issuecomment-399772723)'
   },
   {
-    pattern: /change\s*logs?/,
+    pattern: /change(\s*logs?|s)/,
     message: '[Changelog](https://impactdevelopment.github.io/changelog)'
   },
   {
-    pattern: /damn/,
-    message: 'Damn Daniel!'
-  },
-  {
-    pattern: /op+a/,
-    message: 'OPPA GANGNAM STYLE!'
-  },
-  {
     pattern: /hack(s|ing|er|client)?/,
-    message: 'Please do not discuss h**ks in this Discord.'
-  },
-  {
-    pattern: /1\.13/,
-    message: 'Imminent release. Donate $5 for beta access.'
+    message: 'Please do not discuss hacks in this Discord.'
   }
 ];
