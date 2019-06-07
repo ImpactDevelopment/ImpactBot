79472798f475f7b1e17de2f4ce7fe68a4af2a8a1
ScanmanTM <46611235+ScanmanTM@users.noreply.github.com>
2019-06-07 16:44:06 +0100

    create a new command for Google

---
+++ commands/General/lmgtfy.js
@@ -0,0 +1,31 @@
+    
+const { Command } = require('klasa');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = class extends Command {
+  constructor(...args) {
+    super(...args, {
+      name: 'let me Google that for you :)',
+      enabled: true,
+      runIn: ['text', 'dm', 'group'],
+      cooldown: 3,
+      bucket: 1,
+      aliases: [],
+      permissionLevel: 0,
+      requiredPermissions: ['SEND_MESSAGES', 'EMBED_LINKS'],
+      requiredConfigs: [],
+      description: 'Type a message and the bot will return a url',
+      quotedStringSupport: true,
+      usage: '<search:str>',
+      usageDelim: ' ',
+      extendedHelp: 'No extended help available.'
+    });
+  }
+  
+  async run(msg, [search]) {
+    var searchString = search;
+    var return = searchString.split(' ').join('+')
+    var sendgay = 'http://lmgtfy.com/?q=' + return;
+    msg.send(sendgay);
+  }
+}
