f3c11424b988821eed5c07664e34bd67fc4158c1
Leijurv <leijurv@gmail.com>
2019-07-07 19:33:07 -0700

    Update welcomer.js

--- events/welcomer.js
+++ events/welcomer.js
@@ -13,7 +13,7 @@ module.exports = class extends Event {
       .addField('Usage FAQ', '[Click here!](https://github.com/ImpactDevelopment/ImpactClient/wiki/Usage-FAQ)', true)
       .addField('Rules', '[Click here!](https://discordapp.com/channels/208753003996512258/224684271913140224/306183650268020748)', true)
       .addField('Github Links', '[Impact](https://github.com/ImpactDevelopment/ImpactClient), [Baritone](https://github.com/cabaletta/baritone)')
-      .addField('Downloading and installing the client', '[Click here for tutorial!](https://www.youtube.com/watch?v=k_29vgkPUbk)')
+      .addField('Downloading and installing the client', '[Windows Tutorial](https://youtu.be/i6Wr7t5lF3I) [General Tutorial](https://www.youtube.com/watch?v=k_29vgkPUbk)')
       .setFooter('♿ Impact Client ♿')
     try {
       member.send({ embed })
