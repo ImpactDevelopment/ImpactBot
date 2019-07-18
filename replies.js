b057e06fd2a3872ecca88d2936519e05d9e3b44d
Leijurv <leijurv@gmail.com>
2019-07-18 08:49:03 -0700

    Update replies.js

--- replies.js
+++ replies.js
@@ -26,7 +26,7 @@ module.exports = [
     message: 'https://github.com/cabaletta/baritone/blob/master/USAGE.md'
   },
   {
-    pattern: /installer|mediafire|dire(c|k)+to?\s+(linko?|url|site|page)|adf\.?ly|(ad|u)\s?block/,
+    pattern: /installer|mediafire|dire(c|k)+to?\s+(linko?|url|site|page)|adf\.?ly|(ad|u)\s?block|download/,
     message: '[Direct download link after adfly](https://impactdevelopment.github.io/?brady-money-grubbing-completed=true)'
   },
   {
