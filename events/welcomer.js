d9e4c022bc4c6cc3e7444da8d2fec6aaf77b461c
EmeraldSnorlax <43452521+EmeraldSnorlax@users.noreply.github.com>
2019-07-18 11:35:02 +0100

    Skidded launcher

--- events/welcomer.js
+++ events/welcomer.js
@@ -14,7 +14,7 @@ module.exports = class extends Event {
       .addField('Rules', '[Click here!](https://discordapp.com/channels/208753003996512258/224684271913140224/306183650268020748)', true)
       .addField('Github Links', '[Impact](https://github.com/ImpactDevelopment/ImpactClient), [Installer](https://github.com/ImpactDevelopment/Installer/), [Baritone](https://github.com/cabaletta/baritone)', true)
       .addField('Tutorial videos for downloading and installing the client', '[Windows](https://www.youtube.com/watch?v=QP6CN-1JYYE)\n[Mac OSX](https://www.youtube.com/watch?v=BBO0v4eq95k)\n[Linux](https://www.youtube.com/watch?v=k_29vgkPUbk)\n', true)
-      .addField('Cracked Launchers', 'Impact client is only supported on [MultiMC](https://multimc.org/) and the official Minecraft launcher. That means it won\'t work if you are using a cracked launcher, Technik launcher, or any other launcher. [Just buy the game already.](https://www.minecraft.net/en-us/store/)')
+      .addField('Cracked Launchers', 'Impact client is only supported on [MultiMC](https://multimc.org/) and the official Minecraft launcher. That means it won\'t work if you are using a cracked launcher. [Just buy the game already.](https://www.minecraft.net/en-us/store/)')
       .setFooter('♿ Impact Client ♿')
     try {
       member.send({ embed })
