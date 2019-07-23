bb5db0d1fd8af4941e952df9d75e1ea0b040e185
EmeraldSnorlax <43452521+EmeraldSnorlax@users.noreply.github.com>
2019-07-23 14:36:16 +0100

    wiki updater without bot restart

---
+++ commands/Staff/wikiResync.js
@@ -0,0 +1,42 @@
+const { Command } = require('klasa')
+const { MessageEmbed } = require('discord.js')
+const { exec } = require('child_process')
+const fs = require('fs')
+const rimraf = require('rimraf')
+function capitalizeFirstLetter (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1)
+}
+
+module.exports = class extends Command {
+  constructor (...args) {
+    super(...args, {
+      name: 'wikiResync',
+      enabled: true,
+      runIn: ['text'],
+      cooldown: 30,
+      bucket: 1,
+      aliases: ['wikiupdate', 'wikipull', 'wikisync', 'updatewiki', 'syncwiki'],
+      permissionLevel: 0,
+      requiredPermissions: ['SEND_MESSAGES', 'CHANGE_NICKNAME'], // Lazy way to check for a staff role LMAO
+      requiredConfigs: [],
+      description: 'Sync the bot\'s local copy of the Wiki to the Wiki on Github.',
+      quotedStringSupport: false,
+      usage: '',
+      usageDelim: '',
+      extendedHelp: 'Update the bot\'s local copy of the wiki. Support Role+ only.'
+    })
+  }
+
+  run (msg) {
+    msg.channel.send('Working...')
+    rimraf.sync('./ImpactClient.wiki')
+    exec('git clone https://github.com/ImpactDevelopment/ImpactClient.wiki.git', (err, stdout, stderr) => {
+      if (err) {
+        msg.channel.send('Cloning the wiki repo failed! -- Please try to run this command again as soon as possible, as the bot no longer has a copy of the Wiki!')
+        msg.channel.send('```\n' + err + '\n```')
+      } else {
+        msg.channel.send('Successfully cloned the wiki repository!')
+      }
+    })
+  }
+}
