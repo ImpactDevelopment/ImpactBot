b9b428d13cc2d3777186163c3e6be739025b2378
Leijurv <leijurv@gmail.com>
2019-08-02 16:55:45 -0700

    Update replies.js

--- replies.js
+++ replies.js
@@ -22,7 +22,7 @@ module.exports = [
     message: 'https://www.minecraft.net/en-us/article/screenshotting-guide'
   },
   {
-    pattern: /use\sbaritone|baritone\susage|baritone\scommand|\.b|goal|goto/,
+    pattern: /use\sbaritone|baritone\susage|baritone\scommand|\.b|goal|goto|path/,
     message: 'https://github.com/cabaletta/baritone/blob/master/USAGE.md'
   },
   {
