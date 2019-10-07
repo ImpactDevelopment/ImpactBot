9a4f8b7e655aa973d56c99c7a23e214a99bf3aac
EvilSourcerer <32974335+EvilSourcerer@users.noreply.github.com>
2019-10-07 13:37:00 -0400

    add mute / unmute and clean up rekt.go (#41)

--- rekt.go
+++ rekt.go
@@ -10,6 +10,7 @@ import (
 
 const (
 	RATELIMIT = 5 * time.Minute
+	MUTE_ROLE = "630800201015361566"
 )
 
 var ratelimit = make(map[string]int64)
@@ -37,6 +38,15 @@ func resp(ch string, text string) {
 	discord.ChannelMessageSendEmbed(ch, embed)
 }
 
+func contains(list []string, it string) bool {
+	for _, s := range list {
+		if s == it {
+			return true
+		}
+	}
+	return false
+}
+
 func onMessageSent3(session *discordgo.Session, m *discordgo.MessageCreate) {
 	msg := m.Message
 	if msg == nil || msg.Author == nil || msg.Type != discordgo.MessageTypeDefault || msg.Author.ID == myselfID || m.GuildID != IMPACT_SERVER {
@@ -44,66 +54,50 @@ func onMessageSent3(session *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	content := msg.Content
-	if len(content) < 5 {
-		return
-	}
-	var ban bool
-	if content[:5] == "kick " {
-		ban = false
-	} else {
-		if content[:4] == "ban " {
-			ban = true
-		} else {
-			return
+
+	if strings.HasPrefix(content, "i!") { // bot woke
+		fields := strings.Fields(content[2:])
+		command := strings.ToLower(fields[0])
+		if contains([]string{"kick", "ban", "mute", "unmute"}, command) { // we don't want role checking outside of these commands
+			author, err := GetMember(msg.Author.ID)
+			if err != nil || !isStaff(author) {
+				return
+			}
+			if len(msg.Mentions) != 1 {
+				resp(msg.ChannelID, "Mention exactly one user")
+				return
+			}
+			user := msg.Mentions[0]
+			member, err := GetMember(user.ID)
+			if err != nil {
+				return
+			}
+			if len(member.Roles) > 0 {
+				resp(msg.ChannelID, "They have role(s)")
+				return
+			}
+			if !hasRole(author, STAFF["moderator"], STAFF["developer"]) && !evalRatelimit(msg.Author.ID) {
+				resp(msg.ChannelID, "Too soon")
+				return
+			}
+			providedReason := strings.TrimSpace(content[strings.Index(content, ">")+1:])
+			providedReason = command + " has been issued to " + user.Username + " by @" + msg.Author.Username + "#" + msg.Author.Discriminator + " for reason : " + providedReason
+			switch command {
+			case "ban":
+				err = discord.GuildBanCreateWithReason(m.GuildID, user.ID, providedReason, 0)
+			case "kick":
+				err = discord.GuildMemberDeleteWithReason(m.GuildID, user.ID, providedReason)
+			case "mute":
+				err = discord.GuildMemberRoleAdd(m.GuildID, user.ID, MUTE_ROLE)
+			case "unmute":
+				err = discord.GuildMemberRoleRemove(m.GuildID, user.ID, MUTE_ROLE)
+			}
+			if err != nil {
+				resp(msg.ChannelID, "ERROR "+err.Error())
+				return
+			}
+
+			resp(msg.ChannelID, providedReason)
 		}
 	}
-
-	author, err := GetMember(msg.Author.ID)
-	if err != nil {
-		return
-	}
-	if !isStaff(author) {
-		return
-	}
-	if len(msg.Mentions) != 1 {
-		resp(msg.ChannelID, "Mention exactly one user")
-		return
-	}
-	user := msg.Mentions[0]
-	member, err := GetMember(user.ID)
-	if err != nil {
-		return
-	}
-	if len(member.Roles) > 0 {
-		resp(msg.ChannelID, "They have role(s)")
-		return
-	}
-	if !hasRole(author, STAFF["moderator"], STAFF["developer"]) && !evalRatelimit(msg.Author.ID) {
-		resp(msg.ChannelID, "Too soon")
-		return
-	}
-
-	providedReason := strings.TrimSpace(content[strings.Index(content, ">")+1:])
-
-	providedReason = "by @" + msg.Author.Username + "#" + msg.Author.Discriminator + " for reason: " + providedReason
-
-	text := "@" + user.Username + "#" + user.Discriminator + " was "
-	if ban {
-		text += "banned"
-	} else {
-		text += "kicked"
-	}
-	text += " " + providedReason
-
-	if ban {
-		err = discord.GuildBanCreateWithReason(m.GuildID, user.ID, providedReason, 0)
-	} else {
-		err = discord.GuildMemberDeleteWithReason(m.GuildID, user.ID, providedReason)
-	}
-
-	if err != nil {
-		resp(msg.ChannelID, "ERROR "+err.Error())
-		return
-	}
-	resp(msg.ChannelID, text)
 }
