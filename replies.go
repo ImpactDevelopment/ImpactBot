7b718d7eadc515c05cc370dd69b24eff491e4aba
Arisa <SentinelMKX@gmail.com>
2019-12-15 02:41:48 +0100

    More possible ways to trigger 1.15 reply

--- replies.go
+++ replies.go
@@ -131,7 +131,7 @@ var replies = []Reply{
 		message: "Impact does not support cracked launchers. You can attempt to use the unstable Forge version, but no further support will be provided.",
 	},
 	{
-		pattern: `(1\.15.*?(update|coming|support|release|impact|version|eta|when|out|support)|(update|coming|support|release|impact|version|eta|when|out|support).*?1\.15)`,
+		pattern: `(1\.15.*?(fucking|get|where|need|asap|update|coming|support|release|impact|version|eta|when|out|support)|(fucking|get|where|need|asap|update|coming|support|release|impact|version|eta|when|out|support).*?1\.15)`,
 		message: "No ETA on 1.15 Impact release.",
 	},
 	{
