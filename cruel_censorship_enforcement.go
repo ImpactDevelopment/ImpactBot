5e3e06d32f58ab84edcb6cb94a5b1470d761d28e
XPHonza <XPHonza@users.noreply.github.com>
2020-02-04 21:20:36 +0100

    owo pwease weijuwv senpai

--- cruel_censorship_enforcement.go
+++ cruel_censorship_enforcement.go
@@ -13,7 +13,8 @@ type Censorship struct {
 
 var censor = map[string]Censorship{
 	"563138570953687061": Censorship{"Bella", []string{"kami", "blue"}},
-	"209785549010108416": Censorship{"Arisa", []string{"lewd", "loli", "𝗹𝗲𝘄𝗱", "𝗹𝗼𝗹𝗶", "tenor.com/view/anime-", "tenor.com/view/japanese-cartoon-", "tenor.com/view/gif-anime-"}},
+	"209785549010108416": Censorship{"Arisa", []string{"lewd", "loli", "𝗹𝗲𝘄𝗱", "𝗹𝗼𝗹𝗶", "tenor.com/view/anime-", "tenor.com/view/japanese-cartoon-", "tenor.com/view/gif-anime-", "smh", "boomer"}},
+	"207833493651193856": Censorship("XPHonza", []string{"boomer"}),
 }
 
 func onMessageSent3(session *discordgo.Session, m *discordgo.MessageCreate) {
