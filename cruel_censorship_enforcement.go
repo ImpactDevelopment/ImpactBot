1a4e64a63cc383ab1c72c1e8d4b81ebf95e62c72
Leijurv <leijurv@gmail.com>
2020-07-10 10:16:51 -0700

    Update cruel_censorship_enforcement.go

--- cruel_censorship_enforcement.go
+++ cruel_censorship_enforcement.go
@@ -23,7 +23,7 @@ var globalCensor = Censorship{
 		Explained{"nigg", "a racist slur", "685255238571130891"}, 
 		Explained{"fabritone is better than baritone", "something no one is allowed to say", ""},
 		Explained{"impact is better than future", "something no one is allowed to say", ""},
-	}, setup("kami", "blue", "力ミ", "ブル", "salhack")...),
+	}, setup("kami", "力ミ", "ブル", "salhack")...),
 }
 
 var bannedNicks = []string{
