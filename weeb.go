03bbfdb4bd596fc87da60bf73b2a73af00c8c2ea
ScanmanTM <46611235+ScanmanTM@users.noreply.github.com>
2020-07-13 19:06:30 +0100

    weeb.go

---
+++ weeb.go
@@ -0,0 +1,14 @@
+  
+package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func giveaway(caller *discordgo.Member, msg *discordgo.Message, args []string) error {
+	err := discord.GuildMemberRoleAdd(impactServer, caller.User.ID, "612744883467190275")
+	if err != nil {
+		return err
+	}
+	return discord.MessageReactionAdd(msg.ChannelID, msg.ID, "✅")
+}
