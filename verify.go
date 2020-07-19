62726f8c4aca527c78d005c63e1ea9de85f51018
Leijurv <leijurv@gmail.com>
2020-07-19 13:38:12 -0700

    stupid

--- verify.go
+++ verify.go
@@ -212,7 +212,7 @@ func wantHandler(caller *discordgo.Member, msg *discordgo.Message, args []string
 func DFS(edges map[int][]int, start int, end int) []int {
 	for _, str := range edges[start] {
 		if str == end {
-			return []int{str, end}
+			return []int{start, end}
 		}
 		path := DFS(edges, str, end)
 		if path != nil {
