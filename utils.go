f9aa88a5941dacb5a425ead45e2919d3c295798c
Christian <cesium2349@protonmail.com>
2020-12-13 14:10:22 -0500

    fix pepsi being able to ban (#140)
    * fix pepsi being able to ban
    
    * make formatting less wacky

--- utils.go
+++ utils.go
@@ -69,6 +69,11 @@ func outranks(user1, user2 *discordgo.Member) bool {
 	if role == nil {
 		return IsUserStaff(user1)
 	}
+	
+	if user1 == "96711543202254848" {
+		// pepsi is poo poo and outranks nobody
+		return false
+	}
 
 	return IsUserHigherThan(user1, *role)
 }
