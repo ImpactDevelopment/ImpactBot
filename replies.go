274931d81c9363caf2dd253871103064d884f186
SoNothing <git@sonothing.com>
2020-12-21 20:00:38 +0800

    typo fix

--- replies.go
+++ replies.go
@@ -40,7 +40,7 @@ var replies = []Reply{
 	},
 	{
 		pattern: `defender|virus|mcafee|norton|trojan|\brat\b`,
-		message: "[FAQ: My anitvirus says the installer is a virus! Is it a virus?](https://github.com/ImpactDevelopment/ImpactIssues/wiki/Setup-FAQ#my-antivirus-says-the-installer-is-a-virus-is-it-a-virus)\n\n[Direct download link after AdFly](https://impactdevelopment.github.io/?brady-money-grubbing-completed=true)",
+		message: "[FAQ: My antivirus says the installer is a virus! Is it a virus?](https://github.com/ImpactDevelopment/ImpactIssues/wiki/Setup-FAQ#my-antivirus-says-the-installer-is-a-virus-is-it-a-virus)\n\n[Direct download link after AdFly](https://impactdevelopment.github.io/?brady-money-grubbing-completed=true)",
 	},
 	{
 		pattern: `tutorial|(impact|install|download).*(on|for) (windows|linux|mac)`,
