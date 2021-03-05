8e7e2303b3f7ac260887637902f57c3672f29e67
elonmusksama <56513556+elonmusksama@users.noreply.github.com>
2021-03-05 13:16:57 -0300

    added chess command

---
+++ chess.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func chess(caller *discordgo.Member, msg *discordgo.Message, args []string) error {
+	err := discord.GuildMemberRoleAdd(impactServer, caller.User.ID, Chess.ID)
+	if err != nil {
+		return err
+	}
+	return discord.MessageReactionAdd(msg.ChannelID, msg.ID, check)
+
+	return resp(msg.ChannelID, fmt.Sprintf("User has been given chess role!", caller.User.Username, caller.User.Discriminator))
+}
+
+func unchess(caller *discordgo.Member, msg *discordgo.Message, args []string) error {
+	err := discord.GuildMemberRoleRemove(impactServer, caller.User.ID, Chess.ID)
+	if err != nil {
+		return err
+	}
+	return discord.MessageReactionAdd(msg.ChannelID, msg.ID, check)
+}
