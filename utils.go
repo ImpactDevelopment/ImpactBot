1045560420a39859978b7c1b6d12b6af1b8f8920
elonmusksama <56513556+elonmusksama@users.noreply.github.com>
2021-03-05 18:02:50 -0300

    free pepsi

--- utils.go
+++ utils.go
@@ -70,11 +70,6 @@ func outranks(user1, user2 *discordgo.Member) bool {
 		return IsUserStaff(user1)
 	}
 	
-	if user1.User.ID == "96711543202254848" {
-		// pepsi is poo poo and outranks nobody
-		return false
-	}
-
 	return IsUserHigherThan(user1, *role)
 }
 
